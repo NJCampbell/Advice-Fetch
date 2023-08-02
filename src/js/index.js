7a08185d00c1ee9964d8b0615dc23713aea15664
Nancy Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-08-02 03:08:29 +0000

    need to resolve index.js compiler error

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,30 @@
-//import react into the bundle
-import React from "react";
+
+import { StrictMode } from "react";
+
+import { createRoot } from "react-dom/client";
+
+
+import App from "./app";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+
+// //import react into the bundle
+// import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+// // include your styles into the webpack bundle
+// import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+// //import your own components
+// import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// //render your react application
+// ReactDOM.render(<Home />, document.querySelector("#app"));
